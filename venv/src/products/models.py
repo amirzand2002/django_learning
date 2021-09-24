3e80f2cf6208021578890ae72f5aa93eb481af9d
amir zand <amirzand2002@gmail.com>
2021-09-25 00:30:14 +0330

    making simple model

--- venv/src/products/models.py
+++ venv/src/products/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Product(models.Model):
 	"""docstring for product"""
 
-	title 		=  models.TextField()
-	description = models.TextField()
-	price 		= models.TextField()
-	summary 	= models.TextField(default = ' this is cool!')
+	title 		=  models.CharField(max_length=60)
+	description = models.TextField(blank=True, null= True)
+	price 		= models.DecimalField(decimal_places=2,max_digits =10000)
+	summary 	= models.TextField(blank=False, null= False)
+	featured	= models.BooleanField()
+ 
